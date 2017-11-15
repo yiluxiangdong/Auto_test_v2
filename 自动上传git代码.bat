ce4276ecc47f4d3de8490dd62a0aafca6d96a8d4
unknown <liuxiaobing@xgd.com>
2017-11-15 15:47:33 +0800

    first commit

--- 自动上传git代码.bat
+++ 自动上传git代码.bat
@@ -5,7 +5,8 @@ rem 这一步会在用户目录下的.gitconfig文件最后添加：
 rem [credential]
 rem     helper = store
 rem push 代码
-rem push你的代码 (git push), 这时会让你输入用户名和密码, 这一步输入的用户名密码会被记住, 下次再push代码时就不用输入用户名密码!这一步会在用户目录下生成文件.git-rem credential记录用户名密码的信息。
+rem push你的代码 (git push), 这时会让你输入用户名和密码, 这一步输入的用户名密码会被记住, 下次再push代码时就不用输入用户名密码!
+rem 这一步会在用户目录下生成文件.git-rem credential记录用户名密码的信息。
 rem 总结：
 rem 方案一与方案二都是创建.git-credential文件并写入用户信息，一个是手动创建，一个命令创建。
 
