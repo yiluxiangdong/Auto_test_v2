0b98b328cc6db12cc28cb4afa4d437932536208d
unknown <liuxiaobing@xgd.com>
2017-11-25 15:39:45 +0800

    first commit

--- 资料/RIDE.bat
+++ 资料/RIDE.bat
@@ -1,5 +1,8 @@
-@echo off
-taskkill /f /im pythonw.exe
-del D:\python\python2\Lib\site-packages\robotide\contrib\testrunner\testrunner.pyc
+@echo off 
+@echo 关闭pythonw进程
+tasklist | find /i "pythonw.exe"&&taskkill /f /im pythonw.exe
+@echo 删除testrunner.pyc文件
+if exist "D:\python\python2\Lib\site-packages\robotide\contrib\testrunner\testrunner.pyc" (del D:\python\python2\Lib\site-packages\robotide\contrib\testrunner\testrunner.pyc ) else (echo err)
+@echo 启动RIDE工具
 python  D:\python\python2\Scripts\ride.py
 pause
