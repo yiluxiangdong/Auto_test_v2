da3cf5d97f025afb34e5630af71142becbe6eb82
unknown <liuxiaobing@xgd.com>
2017-11-27 09:41:20 +0800

    first commit

--- 资料/RIDE.bat
+++ 资料/RIDE.bat
@@ -5,4 +5,4 @@ tasklist | find /i "pythonw.exe"&&taskkill /f /im pythonw.exe
 if exist "D:\python\python2\Lib\site-packages\robotide\contrib\testrunner\testrunner.pyc" (del D:\python\python2\Lib\site-packages\robotide\contrib\testrunner\testrunner.pyc ) else (echo err)
 @echo 启动RIDE工具
 python  D:\python\python2\Scripts\ride.py
-pause
+@echo 启动RIDE完成
